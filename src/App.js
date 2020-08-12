58cdde94a9980fb6de03e6ca5ce40af2060e968a
boahs <39522778+boahs@users.noreply.github.com>
2020-08-12 15:52:00 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { BASE_URL, API_KEY } from "./constants/index";
 
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
+      console.log({API_KEY})
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "http://localhost:4000";
+export const API_KEY = "USRuqSU8OT3Szx3l7pP4qxpqEROVq4y5aQH0Pp3p";
